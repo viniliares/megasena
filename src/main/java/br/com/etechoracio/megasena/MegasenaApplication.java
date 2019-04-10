c0d05f6883a68173be67e80214784420201eb9c3
Vinícius Lira <vinicius.soares71@etec.sp.gov.br>
2019-04-10 20:31:43 -0300

    Criação do projeto

---
+++ src/main/java/br/com/etechoracio/megasena/MegasenaApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.megasena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegasenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegasenaApplication.class, args);
+	}
+
+}
